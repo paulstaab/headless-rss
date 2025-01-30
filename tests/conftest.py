dfe3d0da586bad3d89e44db4bf28923d8eab3884
Paul Staab <paulstaab@users.noreply.github.com>
2025-01-30 15:10:33 +0000

    Use local webserver in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+import werkzeug
 from fastapi.testclient import TestClient
 from src import database
 from src.api.nextcloud_news.endpoint import app
@@ -20,3 +21,19 @@ def db(tmp_path: Path):
     database.Base.metadata.create_all(bind=db.get_bind())
     yield db
     db.close()
+
+
+def _respond_with_file(request, file_name: str) -> werkzeug.Response:
+    return werkzeug.wrappers.Response(
+        response=(Path(__file__).parent / file_name).read_text(),
+        status=200,
+        content_type="application/xml",
+    )
+
+
+@pytest.fixture
+def feed_server(httpserver):
+    httpserver.expect_request("/atom.xml").respond_with_handler(
+        lambda x: _respond_with_file(x, "feeds/atom.xml")
+    )
+    return httpserver
