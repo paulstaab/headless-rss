e1b5bf670396df15ae7d95fe5ed4c1243d2ace95
Paul Staab <paulstaab@users.noreply.github.com>
2025-02-01 10:44:19 +0000

    Update CLI

--- src/cli.py
+++ src/cli.py
@@ -1,16 +1,14 @@
-import argparse
+import click
 
 from src.feed import update_all
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Update all feeds in the database.")
-    parser.add_argument("update", action="store_true", help="Update all feeds")
-    args = parser.parse_args()
-
-    if args.update:
+@click.command()
+@click.argument("command", required=True, help="Command to run")
+def cli(command):
+    if command == "update":
         update_all()
 
 
 if __name__ == "__main__":
-    main()
+    cli()
