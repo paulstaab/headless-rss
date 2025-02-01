f797dc1ff53b3c49d4ce48b9a53ee66a55989a34
Paul Staab <paulstaab@users.noreply.github.com>
2025-02-01 18:24:55 +0100

    Improve feed parsing (#26)

--- src/feed.py
+++ src/feed.py
@@ -1,6 +1,7 @@
 import logging
 import time
 from hashlib import md5
+from time import mktime
 
 import feedparser
 
@@ -42,36 +43,47 @@ def update(feed_id: int) -> None:
     logger.info(f"Updating feed {feed_id} ({feed.title})")
     parsed_feed = _parse(feed.url)
     for article in parsed_feed.entries:
-        existing_article = (
-            db.query(database.Article).filter_by(guid_hash=md5(article["id"].encode()).hexdigest()).first()
-        )
+        existing_article = db.query(database.Article).filter_by(guid_hash=_hash(article["id"])).first()
         if not existing_article:
             db.add(_create_article(article, feed_id))
     db.commit()
 
 
 def _create_article(article, feed_id: int) -> database.Article:
+    content: str | None = article.get("content")[0]["value"] if "content" in article else article.get("summary")
+    title: str | None = article.get("title")
+    url: str | None = article.get("link")
+
     return database.Article(
-        title=article.get("title"),
+        title=title,
         author=article.get("author"),
-        body=article.get("summary"),
-        content_hash=article.get("content_hash"),
+        body=content,
+        content=content,
+        content_hash=_hash(content) if content else None,
         enclosure_link=article.get("enclosure_link"),
         enclosure_mime=article.get("enclosure_mime"),
         feed_id=feed_id,
-        fingerprint=article.get("fingerprint"),
+        fingerprint=_create_fingerprint(content, title, url),
         guid=article["id"],
         guid_hash=md5(article["id"].encode()).hexdigest(),
         last_modified=now(),
         media_description=article.get("media_description"),
         media_thumbnail=article.get("media_thumbnail"),
-        pub_date=article.get("pub_date"),
-        rtl=article.get("rtl"),
-        starred=article.get("starred"),
-        unread=article.get("unread"),
-        updated_date=article.get("updated_date"),
-        url=article.get("url"),
+        pub_date=int(mktime(article.get("published_parsed"))),
+        rtl=False,
+        starred=False,
+        unread=True,
+        updated_date=int(mktime(article.get("updated_parsed"))),
+        url=url,
     )
+
+
+def _hash(value: str) -> str:
+    return md5(value.encode()).hexdigest()
+
+
+def _create_fingerprint(content: str | None, title: str | None, url: str | None) -> str:
+    return _hash(_hash(content or "") + _hash(title or "") + _hash(url or ""))
 
 
 def update_all() -> None:
